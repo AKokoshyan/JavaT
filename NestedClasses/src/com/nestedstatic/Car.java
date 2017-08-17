4b7e27109bbbd81eaeaebe27896a89b5e4bc981a
AKokoshyan <akokoshqn@gmail.com>
2017-08-18 00:04:13 +0300

    Performed GIT exercise!

--- NestedClasses/src/com/nestedstatic/Car.java
+++ NestedClasses/src/com/nestedstatic/Car.java
@@ -2,28 +2,15 @@ package com.nestedstatic;
 
 public class Car {
 
-	private String model;
-	private Engine ownEngine;
-
-	public Car(String model) {
-		this.model = model;
-		this.ownEngine = new Engine(110, 2000);
-	}
-
-	public static class Engine {
-
-		private int hP;
-		private int engineVolume;
-
-		public Engine(int hP, int engineVolume) {
-			this.hP = hP;
-			this.engineVolume = engineVolume;
+	void print() {
+		int x = 3;
+		class Guma {
+			void getX() {
+				System.out.println(x);
+			}
 		}
-
-		protected void start() {
-			System.out.println("Brum brum");
-		}
-
+		Guma carGuma = new Guma();
+		carGuma.getX();
 	}
 
 }
--- NestedClasses/src/com/nestedstatic/Demo.java
+++ NestedClasses/src/com/nestedstatic/Demo.java
@@ -1,13 +1,17 @@
 package com.nestedstatic;
 
-import com.nestedstatic.Car.Engine;
+import java.util.LinkedList;
+
+import com.nestedstatic.Car;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		Car ehe = new Car("Peugeot 406");
-		Car.Engine hdi = new Car.Engine(110, 2000);
-		hdi.start();
+		Car ehe = new Car();
+		ehe.print();
+		LinkedList<String> list = new LinkedList<>();
+		list.add("pesho");
+		System.out.println(list.peek());
 	}
 
 }
